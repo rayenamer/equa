92d4e0daa877fcb7bef138ea4b9ea80972399786
rayenamer <rayen.ameur@esprit.tn>
2026-02-14 23:26:50 +0100

    fixed code to pass jobs

--- src/main/java/com/rayen/blockChainManagement/controller/NodeController.java
+++ src/main/java/com/rayen/blockChainManagement/controller/NodeController.java
@@ -18,7 +18,6 @@ import java.util.List;
 @RequestMapping("/api/v1/nodes")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "*")
 public class NodeController {
 
     private final NodeService nodeService;
--- src/main/java/com/rayen/blockChainManagement/model/NodeRequest.java
+++ src/main/java/com/rayen/blockChainManagement/model/NodeRequest.java
@@ -1,7 +1,6 @@
 package com.rayen.blockChainManagement.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
